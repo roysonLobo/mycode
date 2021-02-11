86a63bf06573161debb9f78c0db9a0e68517cccf
roysonLobo <royrenlobo@gmail.com>
2021-02-12 00:56:31 +0530

    first commit

---
+++ football.py
@@ -0,0 +1,20 @@
+football=(input())
+a=0
+b=0
+c=0
+for i in range(len(football)):
+    if (football[i]=='1'):
+        a+=1
+        b=0
+    else:
+        b+=1
+        a=0
+    if (a>=7 or b>=7):
+        c=1
+if c==1:
+    print("YES")
+else:
+    print("NO")
+    
+
+
